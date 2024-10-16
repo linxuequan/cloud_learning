450d1e70fb89f3e119b90212e5918ae4bd5996f3
linxuequan <1399167312@qq.com>
2024-10-16 16:53:42 +0800

    设置一个用于登录校验的GlobalFilter， gateway网关通过获取请求头来得到用户信息，由jwt进行校验。 使用到了AntPathMatcher作路径匹配，这里过滤器还统一设置了未登录返回的401状态码。

---
+++ hm-serviceGateway/src/main/java/com/example/hmservicegateway/filter/AutoGlobalFilter.java
@@ -0,0 +1,61 @@
+package com.example.hmservicegateway.filter;
+
+import com.example.hmservicegateway.config.AuthProperties;
+import com.example.hmservicegateway.utils.JwtTool;
+import com.hmall.common.exception.UnauthorizedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class AutoGlobalFilter implements GlobalFilter, Ordered {
+    private final AuthProperties authProperties;
+    private final JwtTool jwtTool;
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if(isExclude(request.getPath().toString())){
+            return chain.filter(exchange);
+        }
+        String token = null;
+        List<String> headers = request.getHeaders().get("authorization");
+        if(headers != null && !headers.isEmpty()){
+            token = headers.get(0);
+        }
+        Long userId = null;
+        try {
+            userId = jwtTool.parseToken(token);
+        }catch (UnauthorizedException e){
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        System.out.println(userId);
+        return chain.filter(exchange);
+    }
+
+    private boolean isExclude(String path) {
+        for(String pathPattern : authProperties.getExcludePaths()){
+            if(antPathMatcher.match(pathPattern,path))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
