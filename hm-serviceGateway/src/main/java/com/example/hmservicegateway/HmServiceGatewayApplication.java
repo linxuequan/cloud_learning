8624d4e74a5213eab996928b512f5ce341a19025
linxuequan <1399167312@qq.com>
2024-10-16 11:08:38 +0800

    设置网关服务，使用网关转发请求到某些提供服务的服务器对应端口。核心实现在yml文件的配置，包括路径等

---
+++ hm-serviceGateway/src/main/java/com/example/hmservicegateway/HmServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.hmservicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmServiceGatewayApplication.class, args);
+    }
+
+}
